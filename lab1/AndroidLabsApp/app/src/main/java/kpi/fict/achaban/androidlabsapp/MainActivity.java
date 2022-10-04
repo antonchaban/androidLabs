f8bdc54171ddfe8f9a9300a737654ff61062cdf8
Anton Chaban <71810009+antonchaban@users.noreply.github.com>
2022-10-04 11:02:10 +0300

    upd lab1

--- lab1/AndroidLabsApp/app/src/main/java/kpi/fict/achaban/androidlabsapp/MainActivity.java
+++ lab1/AndroidLabsApp/app/src/main/java/kpi/fict/achaban/androidlabsapp/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,13 +19,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button confirmButton = (Button) findViewById(R.id.confirmButton);
-        confirmButton.setOnClickListener(view -> Toast.makeText(MainActivity.this,
-                getValueOfPass(), Toast.LENGTH_LONG).show());
+        confirmButton.setOnClickListener(view -> {
+            TextView myTextView = (TextView) findViewById(R.id.textView2);
+            if (getValueOfPass().isEmpty()) {
+                Toast.makeText(MainActivity.this,
+                        "Enter your password", Toast.LENGTH_LONG).show();
+                myTextView.setText(null);
+            } else {
+
+                myTextView.setText(getValueOfPass());
+            }
+        });
 
         RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
         radioGroup.setOnCheckedChangeListener((group, checkedId) -> {
 
-            EditText passField = (EditText) findViewById(R.id.editTextTextPassword);
+            EditText passField = (EditText) MainActivity.this.findViewById(R.id.editTextTextPassword);
             if (R.id.radioButton == checkedId) {
                 passField.setTransformationMethod(null);
 
